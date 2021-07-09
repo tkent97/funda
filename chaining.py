371e57eeffd4aed95a5e34254c25c3377a6cc793
tkent97 <tristen123abc@gmail.com>
2021-07-09 00:53:35 -0700

    initial commit

---
+++ chaining.py
@@ -0,0 +1,30 @@
+class User:
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        self.account_balance = 0
+    def make_deposit(self, amount):
+        self.account_balance += amount
+
+class User:
+    def make_deposit(self, amount):
+        
+        return self
+
+class User:
+    def make_deposit(self, amount):
+        
+        return self
+
+
+
+guido=User
+
+
+guido.make_deposit(100)
+guido.make_deposit(200)
+# monty.make_deposit(50)
+print(guido.account_balance)
+# print(monty.account_balance)
+
+
